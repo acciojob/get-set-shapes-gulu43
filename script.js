35e0480f1fb47d4ffd4004d6b5c4956f76622d28
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-11 13:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height){
+        if (width<0 || height<0) {
+            console.log("give me val bif than 1")
+        }
+        this._width = width;
+        this._height = height;
+    }
+    get width(){
+        return this._width;
+    }
+    get height(){
+        return this._height;
+    }
 
-class Square extends Animal {}
+    getArea(){
+        return (this._width * this._height);
+    }
 
+}
+
+class Square extends Animal {
+    constructor(side){
+        if (side<0 ) {
+            throw new Error("1>");
+            
+        }
+        super(side,side);
+    }
+    
+
+    getPerimeter(){
+        return 4*_width;
+    }
+}
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
